4484ce38a3474b10f3d015a3dac1d5e90c0134a5
GiovannaL1ma <Giovannaalimarios@gmail.com>
2026-05-13 20:46:08 -0300

    Front-End do perfil do usuário comprador e aplicação do back no dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Encanto Pet - Perfil</title>
     <link rel="stylesheet" href="{{ asset('css/profile/usuario.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/@phosphor-icons/web@2.1.1/src/regular/style.css">
+
 </head>
 <body>
     <header class="navbar">
@@ -30,12 +32,47 @@
                 </div>
 
                 <div class="icons">
-                    <a href="#" alt="Notificação"> <i class="ph ph-bell"></i> </a>
-                    <a href="#"><img src="../../../assets/icon/login.svg" alt="Login"></a>
-                    <a href="#"><img src="../../../assets/icon/mundo.svg" alt="Intercionalização"></a>
+                    <a href="#" alt="Notificação"><i class="ph ph-bell"></i></a>
+                    <a href="#" alt="Login"><i class="ph ph-user"></i></a>
+                    <a href="#" alt="Internacionalização"><i class="ph ph-globe-hemisphere-west"></i></a>
                 </div>
             </div>
         </div>
     </header>
+    <section class="container">
+        <aside class="sidebar">
+            <div class="titulo">
+                <h1>Sua conta Encanto<i class="ph ph-bird"></i></h1>
+            </div>
+
+            <a href="#" alt="Sua conta" class="menu-item active"><i class="ph ph-user"></i>Minha conta</a>
+
+            <a href="#" alt="Notificações" class="menu-item"><i class="ph ph-bell-simple-ringing"></i>Notificações</a>
+
+            <a href="#" alt="Alterar senha" class="menu-item"><i class="ph ph-lock"></i>Alteração de senha</a>
+
+            <a href="#" alt="Seus pedidos" class="menu-item"><i class="ph ph-shopping-cart-simple"></i>Meus pedidos</a>
+
+            <a href="#" alt="Fale conosco" class="menu-item"><i class="ph ph-headset"></i>Fale conosco</a>
+
+            <a href="#" alt="Sair da conta" class="menu-item"><i class="ph ph-sign-out"></i>Sair</a>
+        </aside>
+        <main class="content">
+            <h2>
+                Olá, {{ auth()->user()?->name }}!
+            </h2>
+            <p>Veja tudo sobre a sua conta Encanto.</p>
+            <div class="update-profile" alt="Atualizar perfil">
+            @include('profile.partials.update-profile-information-form', [
+                'user' => auth()->user()
+            ])
+            </div>  
+            <div class="delete-profile" alt="Deletar perfil">
+            @include('profile.partials.delete-user-form', [
+                'user' => auth()->user()
+            ])
+            </div>
+        </main>
+</section>
 </body>
 </html>
